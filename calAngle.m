35e99a337d829f9084d698d810f5854882be82cc
tamjtm <45255184+tamjtm@users.noreply.github.com>
2020-05-26 21:00:41 +0700

    Delete calAngle.m

--- calAngle.m
+++
@@ -1,7 +0,0 @@
-function deg = calAngle(center,left,right)
-    c = pointDistance(left,right);
-    a = pointDistance(left,center);
-    b = pointDistance(center,right);
-    deg = acos( (a^2 + b^2 - c^2)/(2*a*b) ) * 180 * 7 / 22 ;
-end
-
